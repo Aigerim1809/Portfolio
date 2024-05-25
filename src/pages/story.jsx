6d2aa12fefb585f99f4cb07317253da0912ee174
Aigerim <aigerim.madiomarova@narxoz.kz>
2024-05-25 14:35:50 +0600

    ренанеп

--- src/pages/story.jsx
+++ src/pages/story.jsx
@@ -28,35 +28,19 @@ const Story = ( props ) => {
                       <ul>
                           <li className="mil-up">
                               <div className="mil-item-head mil-mb-5">
-                                  <h4>University of Gondar</h4>
-                                  <div className="mil-text-sm">2012 - 2016</div>
+                                  <h4>University of Narxoz</h4>
+                                  <div className="mil-text-sm">2021 - 2025</div>
                               </div>
                               <p className="mil-text-sm mil-mb-15">Student</p>
-                              <p>Dolor sit amet, consectetur adipisicing elit laudantium ipsa ad debitis unde? Iste voluptatibus minus.</p>
+                              <p>Digital Management and Design</p>
                           </li>
                           <li className="mil-up">
                               <div className="mil-item-head mil-mb-5">
-                                  <h4>Mandefro design school</h4>
-                                  <div className="mil-text-sm">2007 - 2013</div>
+                                  <h4>Daryn school</h4>
+                                  <div className="mil-text-sm">2018 - 2021</div>
                               </div>
                               <p className="mil-text-sm mil-mb-15">Student</p>
-                              <p>Dolor sit amet, consectetur adipisicing elit iusto optio, dolorum provident.</p>
-                          </li>
-                          <li className="mil-up">
-                              <div className="mil-item-head mil-mb-5">
-                                  <h4>Web developer courses</h4>
-                                  <div className="mil-text-sm">2013</div>
-                              </div>
-                              <p className="mil-text-sm mil-mb-15">Student</p>
-                              <p>Adipisicing elit iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
-                          </li>
-                          <li className="mil-up">
-                              <div className="mil-item-head mil-mb-5">
-                                  <h4>Academy of Art</h4>
-                                  <div className="mil-text-sm">2015 - 2018</div>
-                              </div>
-                              <p className="mil-text-sm mil-mb-15">Student</p>
-                              <p>Dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+                              <p>Физика Математика</p>
                           </li>
                       </ul>
                   </div>
@@ -74,28 +58,13 @@ const Story = ( props ) => {
                       <ul>
                           <li className="mil-up">
                               <div className="mil-item-head mil-mb-5">
-                                  <h4>Mandefro Studio</h4>
-                                  <div className="mil-text-sm">2018 - 2019</div>
+                                  <h4>Dansai Studio</h4>
+                                  <div className="mil-text-sm">2023 - </div>
                               </div>
-                              <p className="mil-text-sm mil-mb-15">Student</p>
-                              <p>Dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora aste voluptatibus laudantium ipsa ad debitis unde. Iste voluptatibus minus veritatis qui ut.</p>
+                              <p className="mil-text-sm mil-mb-15">Content Maker</p>
+                              <p>На данный момент работаю Контент мейкером</p>
                           </li>
-                          <li className="mil-up">
-                              <div className="mil-item-head mil-mb-5">
-                                  <h4>The Twins PLC</h4>
-                                  <div className="mil-text-sm">2019 - 2021</div>
-                              </div>
-                              <p className="mil-text-sm mil-mb-15">Student</p>
-                              <p>Dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic tempora aste voluptatibus laudantium ipsa ad debitis unde Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora. Laudantium Iste voluptatibus minus veritatis qui ut. Rerum aut hic quasi placeat iure tempora laudantium ipsa Iste voluptatibus minus veritatis qui ut.</p>
-                          </li>
-                          <li className="mil-up">
-                              <div className="mil-item-head mil-mb-5">
-                                  <h4>xyz market</h4>
-                                  <div className="mil-text-sm">2021 - Today</div>
-                              </div>
-                              <p className="mil-text-sm mil-mb-15">Student</p>
-                              <p>Dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut. Dolor sit amet, consectetur adipisicing elit.</p>
-                          </li>
+
                       </ul>
                   </div>
 
